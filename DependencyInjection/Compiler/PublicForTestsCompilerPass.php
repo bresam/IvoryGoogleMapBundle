a0acff2977624b921165aae8f5bc405e5417f370
Rafal Kozlowski <rafal.kozlowski@smarteye.eu>
2024-03-12 23:42:07 +0100

    Fix Compiler pass that is causing errors on strict environments

--- DependencyInjection/Compiler/PublicForTestsCompilerPass.php
+++ DependencyInjection/Compiler/PublicForTestsCompilerPass.php
@@ -17,9 +17,9 @@ class PublicForTestsCompilerPass implements CompilerPassInterface
 
         foreach ($container->getDefinitions() as $definition) {
 
-            if(str_contains($definition->getClass(), 'Ivory\GoogleMapBundle') ||
-               str_contains($definition->getClass(), 'Ivory\GoogleMap')  ||
-               str_contains($definition->getClass(), 'Ivory\Serializer'))
+            if(str_contains((string) $definition->getClass(), 'Ivory\GoogleMapBundle') ||
+               str_contains((string) $definition->getClass(), 'Ivory\GoogleMap')  ||
+               str_contains((string) $definition->getClass(), 'Ivory\Serializer'))
             {
              $definition->setPublic(true);
             }
